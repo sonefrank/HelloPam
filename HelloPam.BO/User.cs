f1b57289a47c2c28eeb95b3a808c329ce3233e20
nkayoum <harissonefares4@gmail.com>
2022-03-31 09:29:18 -0400

    Add project files.

---
+++ HelloPam.BO/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace HelloPam.BO
+{
+    public class User
+    {
+        public int id { get; set; }
+
+        public string  Username { get; set; }
+
+        public string Password { get; set; }
+
+        public string Fullname { get; set; }
+        
+        public int Profile { get; set; }
+
+        public bool Stutus { get; set; }
+
+        public byte[] Picture { get; set; }
+
+        public DateTime CreatedAT { get; set; }
+
+        public User()
+        {
+
+        }
+
+        public User(int id, string username, string password, string fullname, int profile, bool stutus, byte[] picture,DateTime createdAT)
+        {
+            this.id = id;
+            Username = username;
+            Password = password;
+            Fullname = fullname;
+            Profile = profile;
+            Stutus = stutus;
+            Picture = picture;
+            CreatedAT = createdAT;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is User user &&
+                   id == user.id;
+        }
+    }
+}
